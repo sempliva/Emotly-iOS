b0ae2a81d2f453554a9be066c8929863ba9c3b35
Michelangelo De Simone <michel@ngelo.eu>
2016-07-09 21:42:57 -0700

    Fix: Travis CI updated.

--- Emotly/EmotlyTests/EmotlyTests.swift
+++ Emotly/EmotlyTests/EmotlyTests.swift
@@ -24,13 +24,14 @@ class EmotlyTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
